411f5d26a6bae4d3bd67e8a0fc3511d7055a1d1b
Fiser04 <vf6a@seznam.cz>
2025-10-16 21:24:36 +0200

    asd

--- 2.1/JAVA/domaci priprava/Den 31/zadani2/src/main/java/com/mycompany/App.java
+++ 2.1/JAVA/domaci priprava/Den 31/zadani2/src/main/java/com/mycompany/App.java
@@ -6,18 +6,51 @@ public class App {
 
     public static void main(String[] args) {
         boolean nemamInput = true;
+        int inputCislo = -1;
         String input = "";
         try (Scanner sc = new Scanner(System.in)) {
+            //INPUT
             while (nemamInput) {
                 System.out.println("Kolik prvocisel chcete vypsat?");
                 input = sc.nextLine();
+
                 try {
-                    int inputCislo = Integer.parseInt(input);
+                    inputCislo = Integer.parseInt(input);
+                    if (inputCislo > 0) {
+                        nemamInput = false;
+                    } else {
+                        System.out.println("Cislo musi byt vetsi jak 0");
+                    }
                 } catch (Exception e) {
                     System.out.println("Toto neni cislo");
                 }
             }
-            System.out.println(input);
+
+            for (int i : funkce(inputCislo)) {
+                System.out.println(i);
+            }
         } catch (Exception e) {}
+    }
+
+    public static int[] funkce(int inputCislo) {
+        int cisloZkoumane = 2;
+        int[] pole = new int[inputCislo];
+        int pozice = 0;
+        boolean jePrvocislo = true;
+        while (pozice < inputCislo) {
+            jePrvocislo = true;
+            for (int i = 2; i < cisloZkoumane - 1; i++) {
+                if (cisloZkoumane % i == 0) {
+                    jePrvocislo = false;
+                    break;
+                }
+            }
+            if (jePrvocislo) {
+                pole[pozice] = cisloZkoumane;
+                pozice++;
+            }
+            cisloZkoumane++;
+        }
+        return pole;
     }
 }
