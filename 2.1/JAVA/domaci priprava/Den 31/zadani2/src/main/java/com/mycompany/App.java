56a28f51d2b1d19047e91e8b2d4ae234ed4d21f4
Fiser04 <vf6a@seznam.cz>
2025-10-16 21:08:28 +0200

    asd

--- 2.1/JAVA/domaci priprava/Den 31/zadani2/src/main/java/com/mycompany/App.java
+++ 2.1/JAVA/domaci priprava/Den 31/zadani2/src/main/java/com/mycompany/App.java
@@ -5,6 +5,19 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Kolik prvocisel chcete vypsat?");
+        boolean nemamInput = true;
+        String input = "";
+        try (Scanner sc = new Scanner(System.in)) {
+            while (nemamInput) {
+                System.out.println("Kolik prvocisel chcete vypsat?");
+                input = sc.nextLine();
+                try {
+                    int inputCislo = Integer.parseInt(input);
+                } catch (Exception e) {
+                    System.out.println("Toto neni cislo");
+                }
+            }
+            System.out.println(input);
+        } catch (Exception e) {}
     }
 }
