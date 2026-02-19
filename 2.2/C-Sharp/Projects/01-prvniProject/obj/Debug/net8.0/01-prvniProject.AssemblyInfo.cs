34daeec0952dffbcc49822dccf6110ac2392fc16
Fiser04 <vf6a@seznam.cz>
2026-02-19 22:48:19 +0100

    adding c-sharp

--- 2.2/C-Sharp/Projects/01-prvniProject/obj/Debug/net8.0/01-prvniProject.AssemblyInfo.cs
+++ 2.2/C-Sharp/Projects/01-prvniProject/obj/Debug/net8.0/01-prvniProject.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01-prvniProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+045560c7d142c6fa3646ce4d1a62c41b86569db3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca9062c95828199cf78d2fdbac6830a00c5b9474")]
 [assembly: System.Reflection.AssemblyProductAttribute("01-prvniProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01-prvniProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
