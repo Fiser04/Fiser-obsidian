3b77f88f2fc40ea3f7bf37e4f3726243b3f15148
Fiser04 <vf6a@seznam.cz>
2026-02-21 00:53:56 +0100

    we can shoot!

--- Meine things/kurz/Build an Asteroids Game/asteroid-game/main.py
+++ Meine things/kurz/Build an Asteroids Game/asteroid-game/main.py
@@ -3,29 +3,33 @@ import sys
 import pygame
 from pygame.mixer import SoundType
 
-import asteroid
+from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_event, log_state
 from player import Player
+from shot import Shot
 
 
 def main():
-    # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     AsteroidField.containers = updatable
-    asteroid.Asteroid.containers = (asteroids, updatable, drawable)
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     asteroidfield = AsteroidField()
     player1 = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
--- Meine things/kurz/Build an Asteroids Game/asteroid-game/player.py
+++ Meine things/kurz/Build an Asteroids Game/asteroid-game/player.py
@@ -1,7 +1,14 @@
 import pygame
 
 from circleshape import CircleShape
-from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
+from constants import (
+    LINE_WIDTH,
+    PLAYER_RADIUS,
+    PLAYER_SHOT_SPEED,
+    PLAYER_SPEED,
+    PLAYER_TURN_SPEED,
+)
+from shot import Shot
 
 
 class Player(CircleShape):
@@ -34,7 +41,13 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, -1).rotate(self.rotation) * PLAYER_SHOT_SPEED
