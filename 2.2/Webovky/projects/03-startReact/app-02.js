701b7de3f51ac383a83de119ccb5b7a257587ade
Fiser04 <vf6a@seznam.cz>
2026-02-23 10:55:49 +0100

    02

---
+++ 2.2/Webovky/projects/03-startReact/app-02.js
@@ -0,0 +1,9 @@
+// const name = "John";
+// const element = <h1>Hello, {name}</h1>;
+// ReactDOM.render(element, document.getElementById("root"));
+
+const user = {
+  name: "John",
+  age: 30,
+};
+
