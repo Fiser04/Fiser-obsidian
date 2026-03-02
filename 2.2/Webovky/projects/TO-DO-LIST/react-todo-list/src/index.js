172ccb4fdabec881c0c1acaccd9c18016b919491
Fiser04 <vf6a@seznam.cz>
2026-03-02 09:50:31 +0100

    adding contructor

--- 2.2/Webovky/projects/TO-DO-LIST/react-todo-list/src/index.js
+++ 2.2/Webovky/projects/TO-DO-LIST/react-todo-list/src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 class Car extends React.Component {
+  constructor() {
+    super();
+    this.state = { color: "red" };
+  }
   render() {
     return <h2>I am a Car!</h2>;
   }
