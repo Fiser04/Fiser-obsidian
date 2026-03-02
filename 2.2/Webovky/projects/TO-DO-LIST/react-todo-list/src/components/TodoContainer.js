a99d9187d4d8d195f2e8a87685f14bcd33664fd0
Fiser04 <vf6a@seznam.cz>
2026-03-02 10:41:36 +0100

    vypsani z jsonu

--- 2.2/Webovky/projects/TO-DO-LIST/react-todo-list/src/components/TodoContainer.js
+++ 2.2/Webovky/projects/TO-DO-LIST/react-todo-list/src/components/TodoContainer.js
@@ -40,8 +40,13 @@ class TodoContainer extends React.Component {
   render() {
     return (
       <div>
-        <h1>To-Do List</h1>
-        <p>Component created by: Fiser</p>
+        <ul>
+          {this.state.todos.map((todo) => (
+            <li key={todo.id}>
+              {todo.title} - {todo.completed ? "Completed" : "Not completed"}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
