0640e78919c4ead434b55c1cbddc68d236a3bec7
Fiser04 <vf6a@seznam.cz>
2026-03-02 09:49:18 +0100

    volani komponenty v komponente

--- 2.2/Webovky/projects/TO-DO-LIST/react-todo-list/src/index.js
+++ 2.2/Webovky/projects/TO-DO-LIST/react-todo-list/src/index.js
@@ -11,6 +11,18 @@ function Car2() {
   return <h2>I am also a Car!</h2>;
 }
 
+class Garage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Who lives in my garage?</h1>
+        <Car />
+        <Car2 />
+      </div>
+    );
+  }
+}
+
 // const element = <h1>Hello, world!</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Car2 />);
+root.render(<Garage />);
