8b6ccb4ea2a48d2b97c6533cd7e0f4e7d2320c35
Fiser04 <vf6a@seznam.cz>
2026-03-02 09:45:44 +0100

    prvni trida

--- 2.2/Webovky/projects/TO-DO-LIST/react-todo-list/src/index.js
+++ 2.2/Webovky/projects/TO-DO-LIST/react-todo-list/src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const element = <h1>Hello, world!</h1>;
+
+class Car extends React.Component {
+  render() {
+    return <h2>I am a Car!</h2>;
+  }
+}
+
+// const element = <h1>Hello, world!</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+root.render(<Car />);
