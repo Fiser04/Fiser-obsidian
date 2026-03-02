f1a14cdb576ed04de131346c4437e211c377c433
Fiser04 <vf6a@seznam.cz>
2026-03-02 10:00:46 +0100

    Update TodoContainer.js

--- 2.2/Webovky/projects/TO-DO-LIST/react-todo-list/components/TodoContainer.js
+++ 2.2/Webovky/projects/TO-DO-LIST/react-todo-list/components/TodoContainer.js
@@ -1,5 +1,14 @@
-import React from "react"
+import React from "react";
 
 class TodoContainer extends React.Component {
-  
+  render() {
+    return (
+      <div>
+        <h1>To-Do List</h1>
+        <p>Component created by: Fiser</p>
+      </div>
+    );
+  }
 }
+
+export default TodoContainer;
