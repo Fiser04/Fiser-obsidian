380b550741df738ca4681f5a8a25a50af4a1bf35
Fiser04 <vf6a@seznam.cz>
2026-03-02 10:15:44 +0100

    posilani atributu

--- 2.2/Webovky/projects/TO-DO-LIST/react-todo-list/src/index.js
+++ 2.2/Webovky/projects/TO-DO-LIST/react-todo-list/src/index.js
@@ -1,39 +1,37 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import TodoContainer from "./components/TodoContainer";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TodoContainer from "./components/TodoContainer";
 
-/*
 class Car extends React.Component {
-  constructor() {
-    super();
-    this.state = { color: "red" };
-  }
+  // constructor() {
+  //   super();
+  //   this.state = { color: "red" };
+  // }
   render() {
-    return <h2>I am a {this.state.color} Car!</h2>;
+    return <h2>I am a {this.props.brand} Car!</h2>;
   }
 }
 
-function Car2() {
-  return <h2>I am also a Car!</h2>;
-}
+// function Car2() {
+//   return <h2>I am also a Car!</h2>;
+// }
 
 class Garage extends React.Component {
   render() {
     return (
       <div>
         <h1>Who lives in my garage?</h1>
-        <Car />
-        <Car2 />
+        <Car brand="ford" />
+        {/* <Car2 />*/}
       </div>
     );
   }
 }
-
 // const element = <h1>Hello, world!</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Garage />);
-*/
 
+/*
 import React from "react";
 import ReactDOM from "react-dom/client";
 import TodoContainer from "./components/TodoContainer";
@@ -44,3 +42,4 @@ root.render(
     <TodoContainer />
   </React.StrictMode>,
 );
+*/
