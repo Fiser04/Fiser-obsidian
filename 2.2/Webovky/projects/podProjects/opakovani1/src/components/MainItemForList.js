5b279df63f08929ddee96669dbdb12efe516de30
Fiser04 <vf6a@seznam.cz>
2026-03-08 16:28:23 +0100

    Update MainItemForList.js

--- 2.2/Webovky/projects/podProjects/opakovani1/src/components/MainItemForList.js
+++ 2.2/Webovky/projects/podProjects/opakovani1/src/components/MainItemForList.js
@@ -6,7 +6,9 @@ class MainItemForList extends React.Component {
       <div className="main-item-for-list">
         <h3>{this.props.name}</h3>
         <p>{this.props.description}</p>
-        <p>{this.props.read ? "Read" : "Unread"}</p>
+        <p style={{ fontWeight: "bold" }}>
+          {this.props.read ? "Read" : "Unread"}
+        </p>
       </div>
     );
   }
