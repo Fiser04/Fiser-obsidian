b4f7d951f4f873654a49c5c08c0fa997270c133a
Fiser04 <vf6a@seznam.cz>
2026-03-08 16:25:39 +0100

    Update NavBar.js

---
+++ 2.2/Webovky/projects/podProjects/opakovani1/src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+class NavBar extends React.Component {
+  render() {
+    return (
+      <div className="nav-bar">
+        <h1>Navigation Bar</h1>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
