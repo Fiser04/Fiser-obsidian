e97c474ea5cd5329df56deacab378d7bc10df528
Fiser04 <vf6a@seznam.cz>
2026-03-08 16:20:14 +0100

    Update MainList.js

---
+++ 2.2/Webovky/projects/podProjects/opakovani1/src/components/MainList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import MainItemForList from "./MainItemForList.js";
+
+class MainList extends React.Component {
+  render() {
+    return (
+      <div className="main-list">
+        {this.props.items.map((item) => (
+          <MainItemForList
+            key={item.id}
+            name={item.name}
+            description={item.description}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default MainList;
