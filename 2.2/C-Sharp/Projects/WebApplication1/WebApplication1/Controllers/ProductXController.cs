fd022eae69edf9d0cebb0b486b0f98fcfb5a675b
Fiser04 <vf6a@seznam.cz>
2026-03-12 10:12:32 +0100

    sjflasd

--- 2.2/C-Sharp/Projects/WebApplication1/WebApplication1/Controllers/ProductXController.cs
+++ 2.2/C-Sharp/Projects/WebApplication1/WebApplication1/Controllers/ProductXController.cs
@@ -2,7 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Data;
 using WebApplication1.Models;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using WebApplication1.Services;
+using WebApplication1.ViewModels.Products;
 
 namespace WebApplication1.Controllers
 {
@@ -10,20 +12,22 @@ namespace WebApplication1.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly IProductService productsX;
+        private readonly IProductServiceWithDTO productsXService;
 
-        public ProductXController(ApplicationDbContext context, IProductService productService )
+        public ProductXController(ApplicationDbContext context, IProductService productService, IProductServiceWithDTO productsXService)
         {
             _context = context;
             productsX = productService;
+            this.productsXService = productsXService;
         }
 
         // GET: Product
         public async Task<IActionResult> Index()
         {
             //var applicationDbContext = _context.Products.Include(p => p.Category).Include(p => p.Supplier);
-            
+
             // List<Product> products = _context.Products.Where(x => x.Name == "Iphone 1" ).ToList();
-            
+
             // var productsX = from p in products where p.Name == "Iphone 1" select p;
 
             // var productsX = from p in _context.Products select new ViewModels.Products.PruductListDTOOClass()
@@ -33,9 +37,32 @@ namespace WebApplication1.Controllers
             //         CategoryName = p.Category.Name,
             //         SupplierName = p.Supplier.Name
             //     };
-            
-               
+
+
             return View(productsX.GetAll());
+        }
+
+        //products create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
+            ViewData["SupplierId"] = new SelectList(_context.Suppliers, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,CategoryId,SupplierId")] Product product)
+        {
+            ModelState.Clear(); // Clear the ModelState to ensure that validation is performed on the updated model
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                return RedirectToAction(nameof(Index));
+               
+            }
+            return View(product);
         }
     }
 }
--- 2.2/C-Sharp/Projects/WebApplication1/WebApplication1/Services/IProductServiceWithDTO.cs
+++ 2.2/C-Sharp/Projects/WebApplication1/WebApplication1/Services/IProductServiceWithDTO.cs
@@ -1,5 +1,15 @@
+using WebApplication1.ViewModels.Products;
+
 namespace WebApplication1.Services;
 
 public interface IProductServiceWithDTO
 {
+    IEnumerable<ProductListDTO> GetAllWithDetails();
+    ProductDetailDto? GetByIdWithDetails(int id);
+    IEnumerable<ProductListDTO> GetByCategory(int categoryId);
+    IEnumerable<ProductListDTO> GetBySupplier(int supplierId);
+    ProductDetailDto Create(ProductCreateDto dto);
+    void Update(int id, ProductUpdateDto dto);
+    void Delete(int id);
+    bool Exists(int id);
 }
